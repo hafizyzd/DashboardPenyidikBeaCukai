6e0b56902de99fd0940af549b903ba7828b7dc9b
Hafiz Yazid Muhammad <hafizyazidmuhammad@gmail.com>
2025-03-30 20:54:30 +0700

    update login

--- resources/views/Admin/upload.blade.php
+++ resources/views/Admin/upload.blade.php
@@ -16,9 +16,14 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Login</a></li>
+                <li><a class="dropdown-item" href="{{ route('loginuser') }}">Login</a></li>
                 <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#!">Logout</a></li>
+                <li>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="dropdown-item">Logout</button>
+                    </form>
+                </li>
             </ul>
         </li>
     </ul>
--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,15 @@ Route::get('/', function () {
     return redirect()->route('loginuser');
 });
 
-// dashboard
-// Route::get('/exportrekapitulasi', [RekapitulasiController::class, 'rekapitulasiexport'])->name('exportrekapitulasi');
-// Route::get('/dashboard', [RekapitulasiController::class, 'index'])->name('dashboard')->middleware('auth');
-// Route::get('/upload', [RekapitulasiController::class, 'upload'])->name('upload');
-// Route::post('/importrekapitulasi', [RekapitulasiController::class, 'rekapitulasiimportexcel'])->name('importrekapitulasi');
-
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [RekapitulasiController::class, 'index'])->name('dashboard');
     Route::get('/exportrekapitulasi', [RekapitulasiController::class, 'rekapitulasiexport'])->name('exportrekapitulasi');
     Route::get('/upload', [RekapitulasiController::class, 'upload'])->name('upload');
     Route::post('/importrekapitulasi', [RekapitulasiController::class, 'rekapitulasiimportexcel'])->name('importrekapitulasi');
 });
+
 // Login
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('loginuser');
 Route::post('/login', [LoginController::class, 'loginProsess'])->name('loginProsess');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::middleware('auth')->group(function () { 
-    Route::get('/admin/dashboard', function () {
-        return view('Admin.dashboard');
-    })->name('Admin.dashboard');
-});
+Route::middleware('auth')->group(function () { Route::get('/admin/dashboard', function () { return view('Admin.dashboard');})->name('Admin.dashboard');});
