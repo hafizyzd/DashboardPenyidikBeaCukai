d4227d9823ca03c43368ca34588ed79fe5b7b50d
Hafiz Yazid Muhammad <hafizyazidmuhammad@gmail.com>
2025-04-15 14:50:47 +0700

    login repair

--- resources/views/Admin/partials/footeradmin.blade.php
+++ resources/views/Admin/partials/footeradmin.blade.php
@@ -3,9 +3,6 @@
         <div class="d-flex align-items-center justify-content-between small">
             <div>
             <img class="mx-4" src="{{ asset('assets/img/logo_Kemenkeu_BC.png') }}" alt="Logo" style="height: 45px;">
-                <!-- <a href="#">Privacy Policy</a>
-                &middot;
-                <a href="#">Terms &amp; Conditions</a> -->
             </div>
             <div class="text-muted mx-4">Copyright &copy; Subdirektorat Penindakan dan Penyidikan Bea Cukai 2025</div>
         </div>
--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -4,8 +4,9 @@
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="col-md-6">
             <div class="card">
-            <div class="card-header text-center" style="background-color: #205781;background-color: rgba ( 255 , 0 , 0 , 0.4 );"> <h4 style="color: white;">Login</h4> </div>
+            <div class="card-header text-center" style="background-color: #123458;background-color: rgba ( 255 , 0 , 0 , 0.4 );"> <h4 style="color: white;">Login</h4> </div>
                 <div class="card-body">
+                    <img  class="mx-auto d-block mb-2" src="{{ asset('assets/img/logo_Kemenkeu_BC_2.png') }}" alt="Logo" style="height: 65px;">
                     <form method="POST" action="{{ route('loginProsess') }}">
                         @csrf
                         <div class="form-group row mb-2">
@@ -32,7 +33,7 @@
                         </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" style="background-color:#123458; color:#ffffff" class="btn">
                                     {{ __('Login') }}
                                 </button>
                                 @if (Route::has('password.request'))
